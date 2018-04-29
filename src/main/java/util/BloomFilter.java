039a8338cd3454f84af15e6202f7b85ed5dbb219
zruzsik <zoltan.ruzsik@mailbox.tu-dresden.de>
2018-04-29 15:50:30 +0200

    BloomFilterClass von String- auf byte[]-Implementation umgeschrieben

--- src/main/java/util/BloomFilter.java
+++ src/main/java/util/BloomFilter.java
@@ -8,26 +8,26 @@ public class BloomFilter {
 	/**
 	 * Written by Zoltan
 	 * Adds two Bloom filters by bitwise OR and returns the result as String 
-	 * @param s1 Bloom filter in string form
-	 * @param s2 Another Bloom filter in string form
-	 * @return result of bitwise OR of s1 and s2
+	 * @param bf1 1st Bloom filter in byte array form
+	 * @param bf2 2nd Bloom filter in byte array form
+	 * @return result of bitwise OR of the 2 Bloom filters
 	 */
-	public static String add(String s1, String s2) {
-		BigInteger bf1 = new BigInteger(s1.getBytes());
-		BigInteger bf2 = new BigInteger(s2.getBytes());
-		return new String(bf1.or(bf2).toByteArray());
+	public static byte[] add(byte[] bf1, byte[] bf2) {
+		BigInteger bInt1 = new BigInteger(bf1);
+		BigInteger bInt2 = new BigInteger(bf2);
+		return bInt1.or(bInt2).toByteArray();
 	}
 	
 	/**
 	 * Written by Zoltan
 	 * Adds a node's Bloom filter ID to an existing Bloom filter
 	 * @param bfID Bloom filter ID to be added
-	 * @param s Bloom filter in string form
+	 * @param bf Bloom filter in byte array form
 	 * @return result new Bloom filter as String
 	 */
-	public static String addNode(int bfID, String s) {
-		BigInteger filter = new BigInteger(s.getBytes());
-		return new String(filter.setBit(bfID).toByteArray());
+	public static byte[] addNode(int bfID, byte[] bf) {
+		BigInteger filter = new BigInteger(bf);
+		return filter.setBit(bfID).toByteArray();
 	}
 	
 	/**
@@ -43,12 +43,12 @@ public class BloomFilter {
 	 * @return result of reachability query
 	 */
 	
-	public static boolean checkBFReachability(String lin1, String lout1, String lin2, String lout2) {
+	public static boolean checkBFReachability(byte[] lin1, byte[] lout1, byte[] lin2, byte[] lout2) {
 		
-		BigInteger in1 = new BigInteger(lin1.getBytes());
-		BigInteger out1 = new BigInteger(lout1.getBytes());
-		BigInteger in2 = new BigInteger(lin2.getBytes());
-		BigInteger out2 = new BigInteger(lout2.getBytes());
+		BigInteger in1 = new BigInteger(lin1);
+		BigInteger out1 = new BigInteger(lout1);
+		BigInteger in2 = new BigInteger(lin2);
+		BigInteger out2 = new BigInteger(lout2);
 		
 		/* if A.andNot(B) is not empty, there is a bit set in A which is not set in B, meaning A is not a subset of B
 		 * in our case, this check is done for the Bloom filters out2, out1 and in1, in2
