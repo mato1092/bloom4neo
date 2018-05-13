a7458cb2b69eea7bc72e7488427ab1c72560e96c
Sebastian Jannasch <Sebastian.Jannasch@mailbox.tu-dresden.de>
2018-05-13 13:37:11 +0200

    More complex hash functions for BloomFilter

--- src/main/java/bloom4neo/util/BloomFilter.java
+++ src/main/java/bloom4neo/util/BloomFilter.java
@@ -1,15 +1,18 @@
 package bloom4neo.util;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.commons.lang3.StringUtils;
+
 public class BloomFilter {
+	
+	// length of the Lin and Lout value. Must be a Power of 2!
+	private int length = 16;
+	
+	// for uninitialized Lin or Lout values
+	private String init = StringUtils.repeat("0",  length);
 
-	int hash = 8;
-
-	/*
-	 * First implementation of the bloom filter with MOD
-	 * */
-	public BloomFilter() {
-
-	}
 
 	/**
 	 * author mri
@@ -18,13 +21,16 @@ public class BloomFilter {
 	 * @return new value of bloomfilter after adding n
 	 */
 	public String add(String n, String value) {
-		//todo handle it
-		if(value.equals("")){
-			value = "00000000";
-		}
 		int index = this.getIndex(n);
+		
+		//empty value has to be initialized
+		if(value.equals("")){
+			value = init;
+		}
+		
 		StringBuilder result = new StringBuilder(value);
 		result.setCharAt(index, '1');
+		
 		return result.toString();
 	}
 
@@ -37,14 +43,20 @@ public class BloomFilter {
 	 */
 	public boolean check(String n, String value) {
 		int index = this.getIndex(n);
-		//todo same problem
+		
+		//empty value has to be initialized
 		if(value.equals("")){
-			value = "00000000";
+			// TODO: should not be empty at this point, throw Exception?
+			System.out.println("no value to check, maybe there is an Error in the Indexation");
+			value = init;
 		}
+		
 		if (value.charAt(index) == '1') return true;
 		return false;
 	}
 
+	
+	
 	/**
 	 * author mri
 	 * calculate with some fancy hash functions index value
@@ -52,9 +64,133 @@ public class BloomFilter {
 	 * @return index
 	 */
 	private int getIndex(String n) {
-		//todo here doing the real magic
-		return Integer.parseInt(n) % this.hash;
+//		return getIndex_SimpleModuloHash(n);
+//		return getIndex_JavaHash(n);
+//		return getIndex_SHA256Hash(n);
+		return getIndex_Murmur3(n);
 	}
+		
+	
+	
+	
+	/////////// Hashing functions ////////////
+	
+	/**
+	 * Simple Modulo Hash-Function
+	 * 
+	 * @param n String-Value to hash
+	 * @return index for Lin/ Lout
+	 */
+	private int getIndex_SimpleModuloHash(String n) {
+		return Integer.parseInt(n) % this.length;
+	}
+	
+	/**
+	 * Hashes String with intern Java .hashCode() Function. 
+	 * Uses Bit-Wise & to trim the Hash-Value to the length of Lin/Lout.
+	 * 
+	 * @param n String-Value to hash
+	 * @return index for Lin/ Lout
+	 */
+	private int getIndex_JavaHash(String n) {
+		return n.hashCode() & (length -1);
+	}
+	
+	/**
+	 * Encrypt n with SHA 256 and hashes the encrypted String with
+	 * intern Java .hashCode() Function. 
+	 * Uses Bit-Wise & to trim the Hash-Value to the length of Lin/Lout.
+	 * 
+	 * @param n String-Value to hash
+	 * @return index for Lin/ Lout
+	 */
+	private int getIndex_SHA256Hash(String n) {
+		MessageDigest messageDigest;
+		try {
+			messageDigest = MessageDigest.getInstance("SHA-256");
+			messageDigest.update(n.getBytes());
+			String encryptedString = new String(messageDigest.digest());
+			return encryptedString.hashCode() & (length -1);
+			
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return getIndex_JavaHash(n);
+		}
+	}
+	
+	/**
+	 * Uses Murmur3 Hash-Function.
+	 * Uses Bit-Wise & to trim the Hash-Value to the length of Lin/Lout. <br>
+	 * Implementation from: 
+	 * https://github.com/apache/hive/blob/master/storage-api/src/java/org/apache/hive/common/util/Murmur3.java
+	 * 
+	 * @param n String-Value to hash
+	 * @return index for Lin/ Lout
+	 */
+	private int getIndex_Murmur3(String n) {
+		// Constants for 32 bit variant
+		final int C1_32 = 0xcc9e2d51;
+		final int C2_32 = 0x1b873593;
+		final int R1_32 = 15;
+		final int R2_32 = 13;
+		final int M_32 = 5;
+		final int N_32 = 0xe6546b64;
+		
+		final int seed = 0;
+		final int offset = 0;
+		final int stringLength = n.length();
+		final int nBlocks = stringLength >> 2;
+		
+		int hash = seed;
+		byte[] data = n.getBytes();
+		
+		// body
+		for (int i = 0; i < nBlocks; i++) {
+			int i_4 = i << 2;
+			int k = (data[offset + i_4] & 0xff)
+			    | ((data[offset + i_4 + 1] & 0xff) << 8)
+			    | ((data[offset + i_4 + 2] & 0xff) << 16)
+			    | ((data[offset + i_4 + 3] & 0xff) << 24);
+			
+			// mix functions
+		     k *= C1_32;
+		     k = Integer.rotateLeft(k, R1_32);
+		     k *= C2_32;
+		     hash ^= k;
+		     hash = Integer.rotateLeft(hash, R2_32) * M_32 + N_32;	
+		}
+		
+	    // tail
+	    int idx = nBlocks << 2;
+	    int k1 = 0;
+	    switch (stringLength - idx) {
+	      case 3:
+	        k1 ^= data[offset + idx + 2] << 16;
+	      case 2:
+	        k1 ^= data[offset + idx + 1] << 8;
+	      case 1:
+	        k1 ^= data[offset + idx];
+
+	        // mix functions
+	        k1 *= C1_32;
+	        k1 = Integer.rotateLeft(k1, R1_32);
+	        k1 *= C2_32;
+	        hash ^= k1;
+	    }
+
+	    // finalization
+	    hash ^= stringLength;
+	    hash ^= (hash >>> 16);
+	    hash *= 0x85ebca6b;
+	    hash ^= (hash >>> 13);
+	    hash *= 0xc2b2ae35;
+	    hash ^= (hash >>> 16);
+
+	    return hash & (length -1);	
+	
+	}
+	
+	
 
 
 
--- src/test/java/bloom4neo/IndexationTest.java
+++ src/test/java/bloom4neo/IndexationTest.java
@@ -65,22 +65,57 @@ public class IndexationTest
             Session session = driver.session() )
         {
 
-            String create = "create (a:Node{name:1})\n" +
-                    "create (b:Node{name:2})\n" +
-                    "create (c:Node{name:3})\n" +
-                    "create (d:Node{name:4})\n" +
-                    "create (e:Node{name:5})\n" +
-                    "create (f:Node{name:6})\n" +
-                    "create (g:Node{name:7})\n" +
-                    "create (h:Node{name:8})\n" +
-                    "create (i:Node{name:9})\n" +
-                    "create (j:Node{name:10})\n" +
-                    "create (a)-[:HAS]->(b)-[:HAS]->(c)-[:HAS]->(d)\n" +
-                    "create (e)-[:HAS]->(f)-[:HAS]->(g)-[:HAS]->(h)\n" +
-                    "create (i)-[:HAS]->(c)\n" +
-                    "create (j)-[:HAS]->(e)\n" +
-                    "create (i)-[:HAS]->(f)\n" +
-                    "create (g)-[:HAS]->(j)";
+            String create = "Create (n01:NodeCycle { name:'n01' })\n" + 
+            		"Create (n02:NodeCycle { name:'n02' })\n" + 
+            		"Create (n03:NodeCycle { name:'n03' })\n" + 
+            		"Create (n04:NodeCycle { name:'n04' })\n" + 
+            		"Create (n05:NodeNormal { name:'n05' })\n" + 
+            		"Create (n06:NodeNormal { name:'n06' })\n" + 
+            		"Create (n07:NodeNormal { name:'n07' })\n" + 
+            		"Create (n08:NodeNormal { name:'n08' })\n" + 
+            		"Create (n09:NodeNormal { name:'n09' })\n" + 
+            		"Create (n10:NodeNormal { name:'n10' })\n" + 
+            		"Create (n11:NodeNormal { name:'n11' })\n" + 
+            		"Create (n12:NodeCycle { name:'n12' })\n" + 
+            		"Create (n13:NodeCycle { name:'n13' })\n" + 
+            		"Create (n14:NodeCycle { name:'n14' })\n" + 
+            		"Create (n15:NodeCycle { name:'n15' })\n" + 
+            		"Create (n16:NodeCycle { name:'n16' })\n" + 
+            		"Create (n17:NodeCycle { name:'n17' })\n" + 
+            		"Create (n18:NodeCycle { name:'n18' })\n" + 
+            		"Create (n19:NodeNormal { name:'n19' })\n" + 
+            		"Create (n20:NodeNormal { name:'n20' })\n" + 
+            		"Create (n21:NodeNormal { name:'n21' })\n" + 
+            		"Create (n22:NodeNormal { name:'n22' })\n" + 
+            		"Create (n23:NodeNormal { name:'n23' })\n" + 
+            		"Create (n24:NodeCycle { name:'n24' })\n" + 
+            		"Create (n25:NodeCycle { name:'n25' })\n" + 
+            		"Create (n26:NodeNormal { name:'n26' })\n" + 
+            		"Create (n27:NodeCycle { name:'n27' })\n" + 
+            		"Create (n28:NodeCycle { name:'n28' })\n" + 
+            		"Create (n29:NodeNormal { name:'n29' })\n" + 
+            		"Create (n30:NodeNormal { name:'n30' })\n" + 
+            		"Create (n31:NodeCycle { name:'n31' })\n" + 
+            		"Create (n32:NodeCycle { name:'n32' })\n" + 
+            		"Create (n33:NodeCycle { name:'n33' })\n" + 
+            		"\n" + 
+            		"Create (n01)-[:HAS]->(n03)-[:HAS]->(n04)-[:HAS]->(n02)-[:HAS]->(n01),\n" + 
+            		"       (n08)-[:HAS]->(n07)-[:HAS]->(n06),\n" + 
+            		"       (n08)-[:HAS]->(n06)-[:HAS]->(n05)-[:HAS]->(n03),\n" + 
+            		"       (n04)-[:HAS]->(n10)-[:HAS]->(n11)-[:HAS]->(n09),\n" + 
+            		"       (n12)-[:HAS]->(n14)-[:HAS]->(n15)-[:HAS]->(n13)-[:HAS]->(n11),\n" + 
+            		"       (n14)-[:HAS]->(n13),\n" + 
+            		"       (n13)-[:HAS]->(n12),\n" + 
+            		"       (n10)-[:HAS]->(n18),\n" + 
+            		"       (n18)-[:HAS]->(n16)-[:HAS]->(n17)-[:HAS]->(n18),\n" + 
+            		"       (n17)-[:HAS]->(n19),\n" + 
+            		"       (n20)-[:HAS]->(n19),\n" + 
+            		"       (n20)-[:HAS]->(n21)-[:HAS]->(n22),\n" + 
+            		"       (n20)-[:HAS]->(n22),\n" + 
+            		"       (n23)-[:HAS]->(n21),\n" + 
+            		"       (n26)-[:HAS]->(n25)-[:HAS]->(n24)-[:HAS]->(n27)-[:HAS]->(n28)-[:HAS]->(n25),\n" + 
+            		"       (n28)-[:HAS]->(n29),\n" + 
+            		"       (n19)-[:HAS]->(n31)-[:HAS]->(n32)-[:HAS]->(n33)-[:HAS]->(n31)\n";
 
             session.run(create);
             session.run("CALL createIndex");
